cf5b9860356d2b39dcf3aa30bb43dc4ebb6eaf96
Yann Kamche <yannkamche@gmail.com>
2024-05-23 18:35:30 +0100

    added the different Screen stacks for the pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,11 @@
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Link, Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { TouchableOpacity } from 'react-native';
 import 'react-native-reanimated';
 
 export {
@@ -13,11 +16,15 @@ export {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const InitialLayout = () => {
+
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
+
+  // useRouter from expo-router
+  const router = useRouter();
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -34,15 +41,66 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-
   return (
     <Stack>
-      {/* hides the header of the index file */}
+      {/* hides the header of the index page*/}
       <Stack.Screen name="index" options={{ headerShown: false }} />
+
+      {/* Customization on the signup page */}
+      <Stack.Screen
+        name="signup"
+        options={{
+          title: "",
+          headerBackTitle: "",
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: Colors.background },
+          // Replace the custom arrow of a page
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      {/* Customization on the login page */}
+      <Stack.Screen
+        name="login"
+        options={{
+          title: "",
+          headerBackTitle: "",
+          headerShadowVisible: false,
+          headerStyle: { backgroundColor: Colors.background },
+          // Replace the custom arrow of a page
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <Link href={"/help"} asChild>
+              <TouchableOpacity>
+                <Ionicons
+                  name="help-circle-outline"
+                  size={34}
+                  color={Colors.dark}
+                />
+              </TouchableOpacity>
+            </Link>
+          ),
+        }}
+      />
+
+      {/*  customization on the help page */}
+      <Stack.Screen name='help' options={{ title: 'Help', presentation: 'modal'}}/>
     </Stack>
   );
+  
 }
+
+const RootLayoutNav = () => {
+  return (
+    <InitialLayout />
+  )
+}
+
+export default RootLayoutNav
