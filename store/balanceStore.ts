fcc992bce0d6d115b9f6b45b52eb3f2392898586
Yann Kamche <yannkamche@gmail.com>
2024-06-05 06:35:40 +0100

    state management for transactions

---
+++ store/balanceStore.ts
@@ -0,0 +1,45 @@
+import { create } from "zustand";
+import { zustandStorage } from "@/store/mmkv-storage";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+//Defining Transaction
+export interface Transaction {
+  id: string;
+  title: string;
+  amount: number;
+  date: Date;
+}
+
+// Defining Interface for BalanceState
+
+export interface BalanceState {
+  transactions: Array<Transaction>;
+  runTransaction: (transaction: Transaction) => void;
+  balance: () => number;
+  clearTransactions: () => void;
+}
+
+export const useBalanceStore = create<BalanceState>()(
+  persist(
+    (set, get) => ({
+      transactions: [],
+      runTransaction: (transaction: Transaction) => {
+        set((state) => ({
+          transactions: [...state.transactions, transaction],
+        }));
+      },
+      balance: () =>
+        get().transactions.reduce(
+          (acc, transaction) => acc + transaction.amount,
+          0
+        ),
+      clearTransactions: () => {
+        set({ transactions: [] });
+      },
+    }),
+    {
+      name: "balance",
+      storage: createJSONStorage(() => zustandStorage),
+    }
+  )
+);
